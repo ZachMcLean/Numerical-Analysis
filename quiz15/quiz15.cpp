11d8d049e611d90d4b50bf86d9c24a279394dcd4
Zach McLean <zmclean11@gmail.com>
2022-04-14 23:24:11 -0500

    setup 15 && 20

---
+++ quiz15/quiz15.cpp
@@ -0,0 +1,23 @@
+// Zach McLean
+// 4/14/2022
+// 
+
+#include <iostream>
+/* #include <stdlib> */
+#include <cmath>
+
+int main() {
+	// declare four single dimensional arrays
+	int size = 1;
+	double diagonal_arr[size];
+	double upper_arr[size];
+	double lower_arr[size];
+	double b_arr[size];
+	double x = 0;
+	double x1, x2, x3, x4 = 4;
+
+	x = x1 + x2 + x3 + x4;
+
+	printf("%.f %.f %.f %.f", x1, x2, x3, x4);
+	return 0;
+}
