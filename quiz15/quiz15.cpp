6a1e0c71909e30ce14f3e4ed5d3f1a15ac4a40a8
Zach McLean <zmclean11@gmail.com>
2022-04-16 01:17:56 -0500

    added quiz15 & started quiz20

--- quiz15/quiz15.cpp
+++ quiz15/quiz15.cpp
@@ -8,16 +8,90 @@
 
 int main() {
 	// declare four single dimensional arrays
-	int size = 1;
-	double diagonal_arr[size];
-	double upper_arr[size];
-	double lower_arr[size];
+	// size will equal the amount of columns-1 in the array(number of diagnal entries)
+	int size;
+	std::cout<<"Enter num of rows: ";
+	std::cin>>size;
+	
+	double d_arr[size];
+	double u_arr[size-1];
+	double l_arr[size-1];
 	double b_arr[size];
-	double x = 0;
-	double x1, x2, x3, x4 = 4;
+	for(int i = 0; i < 4; i++){
+		if(i == 0){
+			std::cout<<"Please enter diagnal array values seperated by space: ";
+			for(int j = 0; j < size; j++){
+				std::cin>>d_arr[j];
+				/* printf("d[%d]: %.2f \n", j, d_arr[j]); */
+			}
+		}
+		if(i == 1){
+			std::cout<<"Please enter upper array values seperated by space: ";
+			for(int j = 0; j < size-1; j++){
+				std::cin>>u_arr[j];
+				/* printf("d[%d]: %.2f \n", j, d_arr[j]); */
+			}
+		}
+		if(i == 2){
+			std::cout<<"Please enter lower array values seperated by space: ";
+			for(int j = 0; j < size-1; j++){
+				std::cin>>l_arr[j];
+				/* printf("d[%d]: %.2f \n", j, d_arr[j]); */
+			}
+		}	
+		if(i == 3){
+			std::cout<<"Please enter b array values seperated by space: ";
+			for(int j = 0; j < size; j++){
+				std::cin>>b_arr[j];
+				/* printf("d[%d]: %.2f \n", j, d_arr[j]); */
+			}
+		}
+	}
+	std::cout<<"\n";
+	/* double d_arr[] = {1.0, 1.0, 2.0, 7.0}; */
+	/* double u_arr[] = {3.0, -1.0, -1.0}; */
+	/* double l_arr[] = {2.0, 4.0, 5.0}; */
+	/* double b_arr[] = {7.0, 3.0, 8.0, 19.0}; */
+	double x_arr[] = {0.0, 0.0, 0.0, 0.0};
 
-	x = x1 + x2 + x3 + x4;
+	
+	// Forward Elimination for Tri-diagnal System
+	int zero = 0;
+	for(int i = 0; i < size-1; i++){
+		double m = l_arr[i] / d_arr[i];
+		/* std::cout<<"m: "<<m<<"\n"; */
+		l_arr[i] = l_arr[i] - (d_arr[i] * m);
+		d_arr[i+1] = d_arr[i+1] - (u_arr[i] * m);
+		b_arr[i+1] = b_arr[i+1] - (b_arr[i] * m);
+		/* for(int i = 0; i <= size-1; i++){ */
+		/* 	printf("d[%d]: %.2f \n", i, d_arr[i]); */
+		/* 	printf("b[%d]: %.2f \n", i, b_arr[i]); */
+		/* } */
+		/* std::cout<<"\n"; */
+	}
+	// Print the matrix after the forward elimination phase
+	for(int i = 0; i < size; i++){
+		if(i == 0)
+			printf(" | %.2f %.2f %d %d   | %.2f |\n", d_arr[i], u_arr[i], zero, zero, b_arr[i]);
+		if(i == 1)
+			printf(" | %d %.2f %.2f %d | %.2f |\n", zero, d_arr[i], u_arr[i], zero, b_arr[i]);
+		if(i == 2)
+			printf(" | %d %d %.2f %.2f | %.2f |\n", zero, zero, d_arr[i], u_arr[i], b_arr[i]);
+		if(i == 3)
+			printf(" | %d %d %d %.2f   | %.2f |\n", zero, zero, zero, d_arr[i], b_arr[i]);
+	}
 
-	printf("%.f %.f %.f %.f", x1, x2, x3, x4);
+	std::cout<<"\n";
+	// Back Substitution for Tri-diagnal System
+	for(int i = size-1; i >= 0; i--){
+		printf("x[%d]= %.2f / %.2f \n", i+1, b_arr[i], d_arr[i]);
+		if(i == size-1){
+			x_arr[i] = b_arr[i] / d_arr[i];
+			printf("x[%d]: %.2f \n", i+1, x_arr[i]);
+		}else{
+			x_arr[i] = (b_arr[i] - x_arr[i+1] * u_arr[i]) / d_arr[i];
+			printf("x[%d]: %.2f \n", i+1, x_arr[i]);
+		}
+	}
 	return 0;
 }
